27a166c87919ed3992da50bac4b7140c9134f629
kajebiii <kajebiii@gmail.com>
2018-07-06 13:30:52 +0900

    add frontend base (react-redux-saga-webpack

---
+++ frontend/app.js
@@ -0,0 +1,34 @@
+import "babel-polyfill"
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+
+import Counter from './components/Counter'
+import reducer from './store/reducers'
+import rootSaga from './store/sagas'
+
+const sagaMiddleware = createSagaMiddleware()
+const store = createStore(
+  reducer,
+  applyMiddleware(sagaMiddleware)
+)
+sagaMiddleware.run(rootSaga)
+
+const action = type => store.dispatch({type})
+
+function render() {
+    //ReactDOM.render(<HelloWorld />, document.getElementById('app'))
+    ReactDOM.render(
+    <Counter
+        value={store.getState()}
+        onIncrement={() => action('INCREMENT')}
+        onDecrement={() => action('DECREMENT')}
+        onIncrementAsync={() => action('INCREMENT_ASYNC')}/>,
+    document.getElementById('app')
+    )
+}
+
+render()
+store.subscribe(render)
---
+++ frontend/webpack.config.js
@@ -0,0 +1,20 @@
+var webpack = require('webpack')
+var path = require('path')
+module.exports = {
+  entry: ['babel-polyfill', path.resolve(__dirname, 'app')],
+  mode: 'development',
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public')
+  },
+  module: {
+    rules: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader']},
+      {test: /(\.css)$/, loaders: ['style-loader', 'css-loader']}
+    ]
+  }
+}
