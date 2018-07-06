8f08b817dde046d739bc443ba40d5de31ae01c92
kajebiii <kajebiii@gmail.com>
2018-07-06 14:49:18 +0900

    connent component and state

--- frontend/app.js
+++ frontend/app.js
@@ -8,6 +8,7 @@ import createSagaMiddleware from 'redux-saga'
 import CountryList from './components/CountryList'
 import reducers from './store/reducers'
 import sagas from './store/sagas'
+import { initial_country, add_country } from './store/actions'
 
 const sagaMiddleware = createSagaMiddleware()
 const store = createStore(
@@ -30,7 +31,11 @@ function render() {
         }
     }
     ReactDOM.render(
-        <CountryList country_state={country_state}/>,
+        <CountryList 
+            country_state={store.getState().country_state}
+            action_initial_country={() => action(initial_country())}
+            action_add_country={(code, continent, name, capital, phone) => action(add_country(code, continent, name, capital, phone))}
+        />,
         document.getElementById('app')
     )
 }
