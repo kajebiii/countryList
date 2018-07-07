83b62262f2b99a07d7ec66248331d0e918e6251b
kajebiii <kajebiii@gmail.com>
2018-07-07 18:16:05 +0900

    can select row / add css

--- frontend/components/Country.js
+++ frontend/components/Country.js
@@ -1,24 +1,43 @@
-import React, { PropTypes } from 'react'
+import React, { PropTypes, Fragment } from 'react'
 import styled from 'styled-components'
 import { font, palette } from 'styled-theme'
+import Input from './Input'
 
 const Wrapper = styled.div`
   font-family: ${font('primary')};
   color: ${palette('grayscale', 0)};
 `
 
-const Country = ({ country, children, ...props }) => {
-  return (
-    <tr {...props} >
-      <td>{country.code}</td>
-      <td>{country.continent}</td>
-      <td>{country.name}</td>
-      <td>{country.capital}</td>
-      <td>{country.phone}</td>
-      <td></td>
-      {children}
-    </tr>
-  )
+const Country = ({ country, select, children, ...props }) => {
+  if(select === false)
+    return (
+      <Fragment {...props} >
+        <td>{country.code}</td>
+        <td>{country.continent}</td>
+        <td>{country.name}</td>
+        <td>{country.capital}</td>
+        <td>{country.phone}</td>
+        <td></td>
+        {children}
+      </Fragment>
+    )
+  else{
+    let code, continent, name, capital, phone
+    return (
+      <Fragment {...props} >
+        <td><Input type="text" placeholder="code" defaultValue={country.code} innerRef={(ref) => {code = ref;}}></Input></td>
+        <td><Input type="text" placeholder="continent" defaultValue={country.continent} innerRef={(ref) => {continent = ref;}}></Input></td>
+        <td><Input type="text" placeholder="name" defaultValue={country.name} innerRef={(ref) => {name = ref;}}></Input></td>
+        <td><Input type="text" placeholder="capital" defaultValue={country.capital} innerRef={(ref) => {capital = ref;}}></Input></td>
+        <td><Input type="text" placeholder="phone" defaultValue={country.phone} innerRef={(ref) => {phone = ref;}}></Input></td>
+        <td>
+          <button>수정</button>
+          <button>삭제</button>
+        </td>
+        {children}
+      </Fragment>
+    )
+  }
 }
 
 export default Country
--- frontend/components/CountryTbody.js
+++ frontend/components/CountryTbody.js
@@ -8,12 +8,38 @@ const Wrapper = styled.div`
   color: ${palette('grayscale', 0)};
 `
 
-const CountryTbody = ({ countries, children, ...props }) => {
-  return (
-    <tbody {...props}>
-      {countries.map( (country) => <Country key={country.code} country={country}/>)}
-    </tbody>
-  )
+const HoverTr = styled.tr`
+  &:hover {
+    background-color: #bdf;
+  }
+`
+
+const SelectTr = styled.tr`
+  background-color: #fdb;
+`
+
+class CountryTbody extends React.Component {
+  constructor( props ){
+    super(props)
+    this.country_code = -1
+    this.row_click = this.row_click.bind(this)
+  }
+  row_click(country_code) {
+    this.country_code = country_code;
+    this.forceUpdate()
+  }
+  render(){
+    var { countries, children, ...props } = this.props
+    return (
+      <tbody {...props}>
+        {countries.map( (country) => 
+          (country.code === this.country_code) ?
+          (<SelectTr key={country.code} onClick={()=>this.row_click(country.code)}><Country country={country} select={true}/></SelectTr>) :
+          (<HoverTr key={country.code} onClick={()=>this.row_click(country.code)}><Country country={country} select={false}/></HoverTr>)
+        )}
+      </tbody>
+    )
+  }
 }
 
 export default CountryTbody
